d66027989fa11c310a1b36e18026c398ffcec209
sneharb <snehar8552@gmail.com>
2021-03-27 14:48:09 +0530

    first commit

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+    event_name = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    location = models.CharField(max_length=200)
+    is_liked = models.BooleanField(default=False,null=True,blank=False)
+    image = models.ImageField(null=True,blank=False)
+   
+    def __str__(self):
+       return self.event_name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from .forms import EventForm
+from .models import *
+import pyautogui as pu
 
 # Create your views here.
 
 def index(request):
-    context={}
-    return render(request, 'app1/index.html', context)
+    Events = Event.objects.all()
+    context = {'Events': Events}
+    return render(request, 'app1/main.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form=EventForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+        pu.confirm("Your event is registered.Thank you!")
+        return render(request, 'app1/main.html',{'form':form})
+    else:
+        form=EventForm()
+        return render(request, 'app1/register.html',{'form':form})
+
